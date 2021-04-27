38456a54398014ca7fdf47b541ebecee7f51301c
ande19i7 <ande19i7@stud.kea.dk>
2021-04-27 13:00:08 +0200

    header fix

--- page.php
+++ page.php
@@ -19,25 +19,32 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header(); ?>
 
+<style>
+    .elementor-2 .elementor-element.elementor-element-d92a599>.elementor-widget-container {
+        padding: 0px 0px 0px 0px;
+    }
+
+</style>
+
 <?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
 
-	<?php get_sidebar(); ?>
+<?php get_sidebar(); ?>
 
 <?php endif ?>
 
-	<div id="primary" <?php astra_primary_class(); ?>>
+<div id="primary" <?php astra_primary_class(); ?>>
 
-		<?php astra_primary_content_top(); ?>
+    <?php astra_primary_content_top(); ?>
 
-		<?php astra_content_page_loop(); ?>
+    <?php astra_content_page_loop(); ?>
 
-		<?php astra_primary_content_bottom(); ?>
+    <?php astra_primary_content_bottom(); ?>
 
-	</div><!-- #primary -->
+</div><!-- #primary -->
 
 <?php if ( astra_page_layout() == 'right-sidebar' ) : ?>
 
-	<?php get_sidebar(); ?>
+<?php get_sidebar(); ?>
 
 <?php endif ?>
 
